cc8cef7d882f4fb59de8573a09b448e986af16f1
Cedric Brasey <cedric.brasey@planet-side.co.uk>
2025-11-11 09:56:31 +0000

    Minor tweaks
    # Conflicts:
    #	Directory.Packages.props
    #	src/Database/Configuration/UserConfiguration.cs
    #	src/Database/Initialiser.cs

--- src/Database/AuthContext.cs
+++ src/Database/AuthContext.cs
@@ -3,14 +3,19 @@ using Livestock.Auth.Database.Entities;
 
 namespace Livestock.Auth.Database;
 
+using System.Diagnostics.CodeAnalysis;
+
 public class AuthContext(DbContextOptions<AuthContext> options): DbContext(options)
 {
     public virtual DbSet<User> Users { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-       // modelBuilder.HasDefaultSchema(Constants.SchemaName);
+        
+        Requires.NotNull(modelBuilder);
+
+        // modelBuilder.HasDefaultSchema(Constants.SchemaName);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         modelBuilder.HasPostgresExtension(PostgreExtensions.UuidGenerator);
-       
     }
 }
